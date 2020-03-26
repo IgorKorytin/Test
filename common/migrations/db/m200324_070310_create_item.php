d4ee9ea5a9aeda6bf0e923c84ee1b79082dbeeec
IgorKorytin <56302950+IgorKorytin@users.noreply.github.com>
2020-03-26 15:23:54 +0500

    Added migrations for items tables
    Signed-off-by: IgorKorytin <56302950+IgorKorytin@users.noreply.github.com>

---
+++ common/migrations/db/m200324_070310_create_item.php
@@ -0,0 +1,62 @@
+<?php
+
+use yii\db\Expression;
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `item`.
+ */
+class m200324_070310_create_item extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%item}}', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string(512)->notNull(),
+            'description' => $this->text(),
+            'quantity' => $this->integer(),
+            'created_at' => $this->dateTime(),
+            'updated_at' => $this->dateTime()->defaultValue(new Expression('NOW()')),
+            'category_id' => $this->integer(),
+            'category' => $this->string(512)->notNull(),
+        ]);
+
+        $this->createTable('{{%tags}}', [
+            'id' => $this->primaryKey(),
+            'tag' => $this->string(512)->notNull(),
+        ]);
+
+        $this->createTable('{{%category}}', [
+            'id' => $this->primaryKey(),
+            'category' => $this->string(512)->notNull(),
+        ]);
+
+        $this->createTable('{{%item_tags}}', [
+            'id' => $this->primaryKey(),
+            'item_id' => $this->integer(),
+            'tag_id' => $this->integer(),
+            'tag' => $this->string(512)->notNull(),
+        ]);
+
+
+        $this->addForeignKey('fk_item_tag', '{{%item_tags}}', 'item_id', '{{%item}}', 'id', 'cascade', 'cascade');
+        $this->addForeignKey('fk_tag', '{{%item_tags}}', 'tag_id', '{{%tags}}', 'id', 'cascade', 'cascade');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey('fk_item_tag', '{{%item_tags}}');
+        $this->dropForeignKey('fk_tag', '{{%item_tags}}');
+        $this->dropTable('{{%item}}');
+        $this->dropTable('{{%tags}}');
+        $this->dropTable('{{%category}}');
+        $this->dropTable('{{%item_tags}}');
+
+    }
+}
